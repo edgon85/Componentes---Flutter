c350857dbce4caacdef50b131b5a13c74c1c41a1
edgon <edgon85@gmail.com>
2019-05-20 11:04:48 -0600

    Widget Alert

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -7,12 +7,58 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alet Page'),
       ),
+
+      body: Center(
+        child: RaisedButton(
+          /*onPressed: (){
+            _mostrarAlert(context);
+            },*/
+          onPressed: () => _mostrarAlert(context),
+          child: Text('Mostrar Alerta',),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+        ),
+      ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: (){
           Navigator.pop(context);
         },
         child: Icon(Icons.arrow_back),
       ),
+    );
+  }
+
+  void _mostrarAlert(BuildContext context){
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context){
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          title: Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Este es el contenido de la caja de la alerta'),
+              FlutterLogo(size: 100.0,)
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: Text('Cancelar')),
+
+            FlatButton(
+                onPressed: (){
+                  Navigator.of(context).pop();
+                },
+                child: Text('Ok')),
+          ],
+          
+        );
+      }
     );
   }
 }
