156707bbd627c1e0fc634c677fe285f685b07d5f
edgon <edgon85@gmail.com>
2019-05-19 18:12:19 -0600

    Image y FadeInImage

--- lib/src/pages/cards_page.dart
+++ lib/src/pages/cards_page.dart
@@ -10,7 +10,11 @@ class CardPage extends StatelessWidget {
       body: ListView(
         padding: EdgeInsets.all(10.0),
         children: <Widget>[
-          _cardType1()
+          _cardType1(),
+          SizedBox(
+            height: 20.0,
+          ),
+          _cardType2(),
         ],
       ),
     );
@@ -21,16 +25,46 @@ class CardPage extends StatelessWidget {
       child: Column(
         children: <Widget>[
           ListTile(
-            leading: Icon(Icons.photo_album, color: Colors.blue,),
+            leading: Icon(
+              Icons.photo_album,
+              color: Colors.blue,
+            ),
             title: Text("Titluto"),
             subtitle: Text("Esto es un subtitulo de la card"),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
-            FlatButton(onPressed: (){}, child: Text('Cancelar')),
-            FlatButton(onPressed: (){}, child: Text('Ok')),
-          ],)
+              FlatButton(onPressed: () {}, child: Text('Cancelar')),
+              FlatButton(onPressed: () {}, child: Text('Ok')),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _cardType2() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          FadeInImage(
+            placeholder: AssetImage('assets/gif/jar_loading.gif'),
+            image: NetworkImage(
+                'https://static.photocdn.pt/images/articles/2017_1/iStock-545347988.jpg'),
+            fadeInDuration: Duration(milliseconds: 200),
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+
+          /*Image(
+            image: NetworkImage(
+                'https://static.photocdn.pt/images/articles/2017_1/iStock-545347988.jpg'),
+          ),*/
+          Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text(
+                  'Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño'))
         ],
       ),
     );
