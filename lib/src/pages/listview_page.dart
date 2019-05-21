90536c1e2f9135abc5ab6fc4b004546b7f99d35e
edgon <edgon85@gmail.com>
2019-05-20 19:34:59 -0600

    InfiniteScroll con Futures

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class ListPage extends StatefulWidget {
@@ -6,61 +8,110 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
-
   ScrollController _scrollController = new ScrollController();
 
   List<int> _listaNumeros = new List();
   int _ultimoItem = 0;
+
+  bool _isLoading = false;
 
   @override
   void initState() {
     super.initState();
     _agregar10();
 
-    _scrollController.addListener((){
-      if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent){
-        _agregar10();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        // _agregar10();
+        fetchData();
       }
     });
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('ListView page'),
-      ),
-
-      body: _crearLista()
-    );
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
   }
 
-  Widget _crearLista(){
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('ListView page'),
+        ),
+        body: Stack(
+          children: <Widget>[
+            _crearLista(),
+            _crearLoading(),
+          ],
+        ));
+  }
+
+  Widget _crearLista() {
     return ListView.builder(
       controller: _scrollController,
       itemCount: _listaNumeros.length,
-      itemBuilder: (BuildContext contex, int index){
-
+      itemBuilder: (BuildContext contex, int index) {
         final image = _listaNumeros[index];
 
         return Padding(
           padding: const EdgeInsets.all(8.0),
           child: FadeInImage(
-            placeholder: AssetImage('assets/gif/jar_loading.gif'),
-            image: NetworkImage('https://picsum.photos/id/$image/500/300')),
+              placeholder: AssetImage('assets/gif/jar_loading.gif'),
+              image: NetworkImage('https://picsum.photos/id/$image/500/300')),
         );
       },
     );
   }
 
-
-  void _agregar10(){
-    for(var i=0; i<10; i++){
+  void _agregar10() {
+    for (var i = 0; i < 10; i++) {
       _ultimoItem++;
       _listaNumeros.add(_ultimoItem);
     }
-    setState(() {
+    setState(() {});
+  }
 
-    });
+  Future<Null> fetchData() async {
+    _isLoading = true;
+    setState(() {});
+
+    final duration = Duration(seconds: 2);
+    return new Timer(duration, respuestaHTTP);
+  }
+
+  void respuestaHTTP() {
+    _isLoading = false;
+
+    _scrollController.animateTo(
+        _scrollController.position.pixels + 100,
+        duration: Duration(milliseconds: 200),
+        curve: Curves.fastOutSlowIn);
+
+    _agregar10();
+  }
+
+  Widget _crearLoading() {
+    if (_isLoading) {
+      return Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircularProgressIndicator(),
+            ],
+          ),
+          SizedBox(
+            height: 20.0,
+          )
+        ],
+      );
+    } else {
+      return Container();
+    }
   }
 }
